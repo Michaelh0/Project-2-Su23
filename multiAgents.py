69c91df4145f02dd60a614d3743f1d90c20a010c
Michael <michaelh7301@gmail.com>
2023-07-05 22:43:15 -0700

    just q 5 left :)

--- multiAgents.py
+++ multiAgents.py
@@ -74,18 +74,55 @@ class ReflexAgent(Agent):
         newGhostStates = successorGameState.getGhostStates()
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
 
-        manhattanDistance from pacman and ghosts 
+        #print(successorGameState)
+        #print(newFood.asList())
+        #print(newGhostStates)
+        sum = []
+        adding = 0
+        for i in newFood.asList():
+            xy1 = i
+            xy2 = newPos
+            total = abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
+            if total < 4:
+                adding += 1
+            elif total < 12:
+                adding += 0.25
+        #power pellets are o's on the grid
+        ghost = 0
+        ghost_counter = 0
+        for i in newGhostStates:
+            xy1 = i.getPosition()
+            xy2 = newPos
+            total = abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
+            if total < 4:
+                ghost -= 50
+            if newScaredTimes[ghost_counter] > total:
+                ghost += 200
+            ghost_counter += 1
 
-        if new Ghost state is scared:
-            check if distance is 1/2 to get to ghost
-            if so add 500 to score
         
-        check distances from foods 
-            manhattanDistance from pacman and food
-            choose the minimum of the bunch . also thinking of average 
+
+        # for i in newGhostStates:
+        #     xy1 = i.getPosition()
+        #     xy2 = newPos
+        #     total = abs(xy1[0] - xy2[0]) + abs(xy1[1] - xy2[1])
+        #     if total - newScaredTimes:
+        #         ghost -= 100
+
+        return adding + successorGameState.getScore() + ghost
+
+        # manhattanDistance from pacman and ghosts 
+
+        # if new Ghost state is scared:
+        #     check if distance is 1/2 to get to ghost
+        #     if so add 500 to score
         
-        return sum of manhattanDistance1 + extra + manhattanDistance2
-        return successorGameState.getScore()
+        # check distances from foods 
+        #     manhattanDistance from pacman and food
+        #     choose the minimum of the bunch . also thinking of average 
+        
+        # return sum of manhattanDistance1 + extra + manhattanDistance2
+        #return successorGameState.getScore()
 
 def scoreEvaluationFunction(currentGameState: GameState):
     """
@@ -145,13 +182,78 @@ class MinimaxAgent(MultiAgentSearchAgent):
         gameState.isLose():
         Returns whether or not the game state is a losing state
         """
-        helper function (pacman)
-            keeps track of which agents is time to moves
-            returns max of two other functions - two different paths they can take
+        depthCounter = 1
+        numGhosts =  gameState.getNumAgents() - 1
+        depth = self.depth
+        evalFunction = self.evaluationFunction
+        def pacmanMax(gameState2: GameState,deep: int):
+            possibleActions = gameState2.getLegalActions(0)
+            maxValue = 0
+            actionTaken = ""
+            counter = 0
+            #print(possibleActions)
+            for action in possibleActions:
+                if not counter:
+                    maxValue = ghostMin(gameState2.generateSuccessor(0,action),1,deep)
+                    actionTaken = action
+                else:
+                    if maxValue[0] < ghostMin(gameState2.generateSuccessor(0,action),1,deep)[0]:
+                        maxValue = ghostMin(gameState2.generateSuccessor(0,action),1,deep)
+                        actionTaken = action
+                counter = counter + 1
+            #print(maxValue)
+        #     keeps track of which agents is time to moves
+        #     returns max of two other functions - two different paths they can take
+            if not counter:
+                return (evalFunction(gameState2),actionTaken)
+            return (maxValue[0],actionTaken)
 
-        helper function (ghost)
-            keeps track of which agents is time to moves
-            returns min of two other functions - two different paths they can take
+
+        def ghostMin(gameState3: GameState, ghost: int,deep: int):
+        #     keeps track of which agents is time to moves
+        #     returns min of two other functions - two different paths they can take
+            possibleActions = gameState3.getLegalActions(ghost)
+            #stateList = []
+            minValue = 0
+            actionTaken = ""
+            counter = 0
+            # print(gameState.isLose())
+            # print(gameState.isWin())
+            for action in possibleActions:
+                if numGhosts == ghost:
+                    if depth == deep:
+                        if not counter:
+                            minValue = (evalFunction(gameState3.generateSuccessor(ghost,action)),action)
+                            actionTaken = action
+                        else:
+                            if minValue[0] > evalFunction(gameState3.generateSuccessor(ghost,action)):
+                                minValue = (evalFunction(gameState3.generateSuccessor(ghost,action)),action)
+                                actionTaken = action
+                    else:
+                        if not counter:
+                            minValue = pacmanMax(gameState3.generateSuccessor(ghost,action),deep+1)
+                            actionTaken = action
+                        else:
+                            if minValue[0] > pacmanMax(gameState3.generateSuccessor(ghost,action),deep+1)[0]:
+                                minValue = pacmanMax(gameState3.generateSuccessor(ghost,action),deep+1)
+                                actionTaken = action
+                else:
+                    if not counter:
+                        minValue = ghostMin(gameState3.generateSuccessor(ghost,action),ghost+1,deep)
+                        actionTaken = action
+                    else:
+                        if minValue[0] > ghostMin(gameState3.generateSuccessor(ghost,action),ghost+1,deep)[0]:
+                            minValue = ghostMin(gameState3.generateSuccessor(ghost,action),ghost+1,deep)
+                            actionTaken = action
+                counter+=1
+            #print((minValue,actionTaken),minValue)
+            if not counter:
+                return (evalFunction(gameState3),actionTaken)
+            return (minValue[0],actionTaken)
+        
+        return pacmanMax(gameState,1)[1]
+
+        #need to now return a list of the actions that lead to this best outcome
 
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
@@ -163,8 +265,82 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """
         Returns the minimax action using self.depth and self.evaluationFunction
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        depthCounter = 1
+        numGhosts =  gameState.getNumAgents() - 1
+        depth = self.depth
+        evalFunction = self.evaluationFunction
+        def pacmanMax(gameState2: GameState,deep: int,alpha: float,beta: float):
+            possibleActions = gameState2.getLegalActions(0)
+            maxValue = 0
+            actionTaken = ""
+            counter = 0
+            #print(possibleActions)
+            for action in possibleActions:
+                if not counter:
+                    maxValue = ghostMin(gameState2.generateSuccessor(0,action),1,deep,alpha,beta)
+                    actionTaken = action
+                else:
+                    if maxValue[0] < ghostMin(gameState2.generateSuccessor(0,action),1,deep,alpha,beta)[0]:
+                        maxValue = ghostMin(gameState2.generateSuccessor(0,action),1,deep,alpha,beta)
+                        actionTaken = action
+                if maxValue[0] > beta:
+                    return (maxValue[0],actionTaken) 
+                alpha = max([alpha,maxValue[0]])
+                counter = counter + 1
+            #print(maxValue)
+        #     keeps track of which agents is time to moves
+        #     returns max of two other functions - two different paths they can take
+            if not counter:
+                return (evalFunction(gameState2),actionTaken)
+            return (maxValue[0],actionTaken)
+
+
+        def ghostMin(gameState3: GameState, ghost: int,deep: int,alpha: float,beta: float):
+        #     keeps track of which agents is time to moves
+        #     returns min of two other functions - two different paths they can take
+            possibleActions = gameState3.getLegalActions(ghost)
+            #stateList = []
+            minValue = 0
+            actionTaken = ""
+            counter = 0
+            # print(gameState.isLose())
+            # print(gameState.isWin())
+            for action in possibleActions:
+                if numGhosts == ghost:
+                    if depth == deep:
+                        if not counter:
+                            minValue = (evalFunction(gameState3.generateSuccessor(ghost,action)),action)
+                            actionTaken = action
+                        else:
+                            if minValue[0] > evalFunction(gameState3.generateSuccessor(ghost,action)):
+                                minValue = (evalFunction(gameState3.generateSuccessor(ghost,action)),action)
+                                actionTaken = action
+                    else:
+                        if not counter:
+                            minValue = pacmanMax(gameState3.generateSuccessor(ghost,action),deep+1,alpha,beta)
+                            actionTaken = action
+                        else:
+                            if minValue[0] > pacmanMax(gameState3.generateSuccessor(ghost,action),deep+1,alpha,beta)[0]:
+                                minValue = pacmanMax(gameState3.generateSuccessor(ghost,action),deep+1,alpha,beta)
+                                actionTaken = action
+                else:
+                    if not counter:
+                        minValue = ghostMin(gameState3.generateSuccessor(ghost,action),ghost+1,deep,alpha,beta)
+                        actionTaken = action
+                    else:
+                        if minValue[0] > ghostMin(gameState3.generateSuccessor(ghost,action),ghost+1,deep,alpha,beta)[0]:
+                            minValue = ghostMin(gameState3.generateSuccessor(ghost,action),ghost+1,deep,alpha,beta)
+                            actionTaken = action
+                if minValue[0] < alpha:
+                    return (minValue[0],actionTaken) 
+                beta = min([beta,minValue[0]])
+                counter+=1
+            #print((minValue,actionTaken),minValue)
+            if not counter:
+                return (evalFunction(gameState3),actionTaken)
+            return (minValue[0],actionTaken)
+        
+        return pacmanMax(gameState,1,float("-inf"),float("inf"))[1]
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
     """
@@ -178,8 +354,61 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         All ghosts should be modeled as choosing uniformly at random from their
         legal moves.
         """
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        depthCounter = 1
+        numGhosts =  gameState.getNumAgents() - 1
+        depth = self.depth
+        evalFunction = self.evaluationFunction
+        def pacmanMax(gameState2: GameState,deep: int):
+            possibleActions = gameState2.getLegalActions(0)
+            maxValue = 0
+            actionTaken = ""
+            counter = 0
+            #print(possibleActions)
+            for action in possibleActions:
+                if not counter:
+                    maxValue = ghostMin(gameState2.generateSuccessor(0,action),1,deep)
+                    actionTaken = action
+                else:
+                    if maxValue[0] < ghostMin(gameState2.generateSuccessor(0,action),1,deep)[0]:
+                        maxValue = ghostMin(gameState2.generateSuccessor(0,action),1,deep)
+                        actionTaken = action
+                counter = counter + 1
+            #print(maxValue)
+        #     keeps track of which agents is time to moves
+        #     returns max of two other functions - two different paths they can take
+            if not counter:
+                return (evalFunction(gameState2),actionTaken)
+            return (maxValue[0],actionTaken)
+
+
+        def ghostMin(gameState3: GameState, ghost: int,deep: int):
+        #     keeps track of which agents is time to moves
+        #     returns min of two other functions - two different paths they can take
+            possibleActions = gameState3.getLegalActions(ghost)
+            #stateList = []
+            minValue = 0
+            actionTaken = []
+            counter = 0
+            # print(gameState.isLose())
+            # print(gameState.isWin())
+            for action in possibleActions:
+                if numGhosts == ghost:
+                    if depth == deep:
+                        minValue += evalFunction(gameState3.generateSuccessor(ghost,action))
+                        actionTaken = actionTaken + [action]
+                    else:
+                        minValue += pacmanMax(gameState3.generateSuccessor(ghost,action),deep+1)[0]
+                        actionTaken = actionTaken + [action]
+                else:
+                    minValue += ghostMin(gameState3.generateSuccessor(ghost,action),ghost+1,deep)[0]
+                    actionTaken = actionTaken + [action]
+                counter+=1
+            #print((minValue,actionTaken),minValue)
+            if not counter:
+                return (evalFunction(gameState3),actionTaken)
+            return (minValue/(counter),random.choice(actionTaken))
+        
+        return pacmanMax(gameState,1)[1]
 
 def betterEvaluationFunction(currentGameState: GameState):
     """
